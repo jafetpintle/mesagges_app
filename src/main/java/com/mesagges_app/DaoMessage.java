8601b3b2448738752c2af2011b1936b1049919f5
jafet <jafetpintle@gmail.com>
2023-02-16 22:10:35 -0600

    Create messagges function from DAOMessage

--- src/main/java/com/mesagges_app/DaoMessage.java
+++ src/main/java/com/mesagges_app/DaoMessage.java
@@ -1,8 +1,28 @@
 package com.mesagges_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DaoMessage {
+    static Conexion con = Conexion.getInstance();
+
     public static void createMessage(Message message){
-        
+        try(Connection connection = con.getConnection()){
+            PreparedStatement ps = null;
+            try{
+                String query="INSERT INTO `mensajes_app`.`messages` (`message`, `message_author`) VALUES (?,?)";
+                ps = connection.prepareStatement(query);
+                ps.setString(1,message.getContent());
+                ps.setString(2,message.getAuthor());
+                ps.executeUpdate();
+                System.out.printf("Message was created succesfull!");
+            }catch(SQLException ex){
+                System.out.printf("Error creating message\n"+ex);
+            }
+        }catch(SQLException e){
+            System.out.println("Error connection to DB\n"+e);
+        }
     }
 
     public static void readMessage(){
--- src/main/java/com/mesagges_app/ServiceMessage.java
+++ src/main/java/com/mesagges_app/ServiceMessage.java
@@ -1,8 +1,21 @@
 package com.mesagges_app;
 
+import java.util.Scanner;
+
 public class ServiceMessage {
     public static void createMessage(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write your message");
+        String messageContent = sc.nextLine();
 
+        System.out.println("Write your name");
+        String messageName = sc.nextLine();
+
+        Message ms = new Message();
+        ms.setContent(messageContent);
+        ms.setAuthor(messageName);
+
+        DaoMessage.createMessage(ms);
     }
 
     public static void listMessages(){
